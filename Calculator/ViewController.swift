10f5c35e07dc84ec98851e0efff0dcd71ea7dd9e
student <student@students-mac.student.georgianc.on.ca>
2016-02-04 16:10:47 -0500

    Finished all calculations and checks

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,47 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Variables
     var operationString = String()
+    var validOperators: Array<Character> = ["x","+","-","÷"]
+    var isFirst = true
     @IBOutlet weak var calculatorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    //Check if the last character in the operation string is a operator, if so then return true
+    func checkOperator() -> Bool
+    {
+        if(!operationString.isEmpty)
+        {
+            for j in validOperators
+            {
+                if operationString[operationString.endIndex.predecessor()] == j
+                {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+    //When the reset button is clicked, reset the text to 0
     @IBAction func resetButtonClicked(sender: AnyObject) {
         operationString = ""
         calculatorLabel.text = "0"
-        
+        isFirst = true
     }
     @IBAction func buttonClicked(sender: AnyObject) {
-        if(sender.currentTitle != "=")
+        var isOperator = false
+        
+        for j in validOperators
+        {
+                if(Character(sender.currentTitle!!) == j)
+                {
+                    isOperator = true
+                }
+        }
+        
+        if(sender.currentTitle != "=" && ((isOperator && !checkOperator() && !isFirst) || !isOperator || (isOperator && !isFirst)))
         {
             operationString = operationString + sender.currentTitle!!
+            calculatorLabel.text = operationString
+            isFirst = false
+        }
+        else
+        {
+            calculatorLabel.text = "0"
         }
         
-        calculatorLabel.text = operationString
         
-        if(sender.currentTitle == "=")
+        
+        if(sender.currentTitle == "="  && !checkOperator() && !operationString.isEmpty)
         {
-            var fixedString = operationString.stringByReplacingOccurrencesOfString("x", withString:"*")
-            fixedString = fixedString.stringByReplacingOccurrencesOfString("÷", withString: "/")
-            let expn = NSExpression(format:fixedString)
-            var result = Float();
-            result = expn.expressionValueWithObject(nil, context: nil) as! Float
-            print(result)
-            calculatorLabel.text = String(result)
-            operationString = calculatorLabel.text!
+                var fixedString = operationString.stringByReplacingOccurrencesOfString("x", withString:"*")
+                fixedString = fixedString.stringByReplacingOccurrencesOfString("÷", withString: "/")
+                let expn = NSExpression(format:fixedString)
+                var result = Float();
+                result = expn.expressionValueWithObject(nil, context: nil) as! Float
+                calculatorLabel.text = String(result)
+                operationString = calculatorLabel.text!
         }
     }
-   
-    
-    
-    
-
 }
 
